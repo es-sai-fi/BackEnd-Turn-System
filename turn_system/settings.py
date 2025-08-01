b5f251006f292ef63d65fc0f750146e511db124c
es-sai-fi <96452903+es-sai-fi@users.noreply.github.com>
2025-07-31 20:52:32 -0500

    Update settings.py

--- turn_system/settings.py
+++ turn_system/settings.py
@@ -138,9 +138,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
-LOGIN_REDIRECT_URL = 'home'
-LOGOUT_REDIRECT_URL = 'home'
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # This production code might break development mode, so we check whether we're in DEBUG mode
 if not DEBUG:
